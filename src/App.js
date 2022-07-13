1202b97ecb095a05319a36771d205897be358f73
Dawid <dawid@pluscode.io>
2022-07-13 16:20:27 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart"
+import CartProvider from "./store/CartProvider";
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false)
+
+  const toggleCart = () => {
+    setCartIsShown(prevValue => !prevValue)
+  }
+
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClick={toggleCart}/>}
+      <Header onClick={toggleCart}/>
       <main>
         <Meals/>
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
